8e43a9f6ccb4690ec61634853fb5972139d28bf6
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-11-28 22:38:00 +0300

    Navigation Buttons' responsive design has been fixed.

--- src/Components/NavigationButtons/index.js
+++ src/Components/NavigationButtons/index.js
@@ -10,31 +10,31 @@ import Button from 'react-bootstrap/Button';
 function NavigationButtons() {
   return (
     <Container className="py-5">
-      <Row className="justify-content-between">
-        <Col xs={2}>
+      <Row className="justify-content-center">
+        <Col xs={5} md={3} className="pb-3 pb-md-0">
           <Link to="/Concerts">
-              <Button variant="warning" size="lg">
+              <Button variant="warning" size="lg" className="w-100">
                 Concerts
               </Button>
           </Link>
         </Col>
-        <Col xs={3} className="text-center">
+        <Col xs={5} md={3}>
           <Link to="/Theaters">
-              <Button variant="warning" size="lg">
+              <Button variant="warning" size="lg" className="w-100">
                 Theaters
               </Button>
           </Link>
         </Col>
-        <Col xs={3} className="text-center">
+        <Col xs={5} md={3}>
           <Link to="/Festivals">
-              <Button variant="warning" size="lg">
+              <Button variant="warning" size="lg" className="w-100">
                 Festivals
               </Button>
           </Link>
         </Col>
-        <Col xs={2} className="text-end">
+        <Col xs={5} md={3} className="text-lg-center">
           <Link to="/Standups">
-              <Button variant="warning" size="lg">
+              <Button variant="warning" size="lg" className="w-100">
                 Standups
               </Button>
           </Link>
