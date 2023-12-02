2c3504dec37e9b55edd2b6d7b269587db873fdfb
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-12-02 18:15:22 +0300

    Added responsive design to FilterBar component. The responsive design of navigation buttons and searchbar components has been changed.

--- src/Components/FilterBar/index.js
+++ src/Components/FilterBar/index.js
@@ -1,59 +1,57 @@
 import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-function FilterBar({CityOptions, LocationOptions, handleClearButton, city, location, startDate, endDate, cityHandleChange, locationHandleChange, startDateHandleChange, endDateHandleChange, filterHandleSubmit}) {
+function FilterBar({ CityOptions, LocationOptions, handleClearButton, city, location, startDate, endDate, cityHandleChange, locationHandleChange, startDateHandleChange, endDateHandleChange, filterHandleSubmit }) {
   return (
-    <Container className="my-5 p-4 bg-dark rounded-pill">
-              <Row>
-                <Col xs={12}>
-                  <form onSubmit={filterHandleSubmit}>
-                    <Row className="align-items-center">
-                      <Col xs={3}>
-                        <select
-                          value={city}
-                          onChange={cityHandleChange}
-                          className="w-100 form-control-lg rounded-pill">
-                          <option value="" defaultValue>
-                            Cities...
-                          </option>
-                          {CityOptions()}
-                        </select>
-                      </Col>
-                      <Col xs={3}>
-                        <select
-                          value={location}
-                          onChange={locationHandleChange}
-                          className="w-100 form-control-lg rounded-pill">
-                          <option value="" defaultValue>Mekanlar...</option>
-                          {LocationOptions()}
-                        </select>
-                      </Col>
-                      <Col xs={2}>
-                        <input
-                          type='date'
-                          value={startDate ? startDate : new Date()}
-                          onChange={startDateHandleChange}
-                          className="w-100 form-control form-control-lg rounded-pill" />
-                      </Col>
-                      <Col xs={2}>
-                        <input
-                          type='date'
-                          value={endDate ? endDate : new Date()}
-                          onChange={endDateHandleChange}
-                          placeholder='DD-MM-YYYY'
-                          className="w-100 form-control form-control-lg rounded-pill" />
-                      </Col>
-                      <Col>
-                        <Button type="submit" variant="warning">Search</Button>
-                      </Col>
-                      <Col>
-                        <Button type="button" onClick={handleClearButton} variant="danger" size="lg">Clear</Button>
-                      </Col>
-                    </Row>
-                  </form>
-                </Col>
-              </Row>
-            </Container>
+    <Container fluid="lg">
+      <Container className="bg-dark mt-5 mt-xl-3 mb-5 p-3 rounded-5">
+        <Form onSubmit={filterHandleSubmit}>
+          <Row className="align-items-center">
+            <Col xs={6} xl={2} className="mb-3 mb-xl-0">
+              <select
+                value={city}
+                onChange={cityHandleChange}
+                className="w-100 form-control-lg rounded-pill">
+                <option value="" defaultValue>
+                  Cities...
+                </option>
+                {CityOptions()}
+              </select>
+            </Col>
+            <Col xs={6} xl={2} className="mb-3 mb-xl-0">
+              <select
+                value={location}
+                onChange={locationHandleChange}
+                className="w-100 form-control-lg rounded-pill">
+                <option value="" defaultValue>Mekanlar...</option>
+                {LocationOptions()}
+              </select>
+            </Col>
+            <Col xs={6} xl={2} className="mb-3 mb-xl-0">
+              <input
+                type='date'
+                value={startDate ? startDate : new Date()}
+                onChange={startDateHandleChange}
+                className="w-100 form-control form-control-lg rounded-pill" />
+            </Col>
+            <Col xs={6} xl={2} className="mb-3 mb-xl-0">
+              <input
+                type='date'
+                value={endDate ? endDate : new Date()}
+                onChange={endDateHandleChange}
+                placeholder='DD-MM-YYYY'
+                className="w-100 form-control form-control-lg rounded-pill" />
+            </Col>
+            <Col xs={6} xl={2}>
+              <Button type="submit" variant="warning" className="w-100 rounded-pill p-2">Filter</Button>
+            </Col>
+            <Col xs={6} xl={2}>
+              <Button type="button" onClick={handleClearButton} variant="warning" className="w-100 rounded-pill p-2">Clear</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </Container>
   )
 }
 
