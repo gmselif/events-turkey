0dd845b75db075f01c350bbe333611023e4928e4
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-12-03 16:52:02 +0300

    Detail Page's responsive design has been changed.

--- src/Pages/Details/index.js
+++ src/Pages/Details/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from "react-query";
 import GetById from '../../Network/GetById'
 import { useParams } from 'react-router-dom'
 import Slider from '../../Components/Slider'
 import ShareButtons from '../../Components/ShareButtons';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
 
 function Details() {
   const { id } = useParams();
@@ -25,36 +25,116 @@ function Details() {
     },
   ]
 
+
+  const [fixed, setFixed] = useState(true)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+
+    if (scrolled <= 400) {
+      setFixed(true)
+    } else if (scrolled > 400) {
+      setFixed(false)
+    }
+  };
+  window.addEventListener('scroll', toggleVisible);
+
+  const [value, setValue] = useState("")
+
+
   return (
-    <div style={{ marginTop: "10vh" }}>
+    <>
       {status === "error" && <p>Error fetching data</p>}
       {status === "loading" && <p>Fetching data...</p>}
       {status === "success" && (
-        <Container>
-          <Row>
-            <Col sm={6}>
-              <Slider pictures={data[0].images} />
+        <Container className="mt-5">
+          <Row className="gap-5">
+            <Col xs={3}>{/*Picture*/}
+              {fixed ? (
+                <Row className='bg-black w-100'>
+                  <Col xs={3} className="top-5 position-fixed">
+                    <Slider pictures={data[0].images} />
+                  </Col>
+                </Row>
+              ) : (
+                <Row className="align-items-baseline">
+                  <Col xs={3} className="top-5" >
+                    <Slider pictures={data[0].images} />
+                  </Col>
+                </Row>
+              )
+              }
             </Col>
-          </Row>
-          <Row>
-            <Col>
-              <ShareButtons eventName={data[0].name} eventType={data[0].eventType} eventDescription={data[0].description} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <iframe
-                src={(googleMap.find(item => item.city.toLowerCase() === data[0].city.toLowerCase())).url}
-                width="600"
-                height="450"
-                style={{ border: "0" }}
-                loading="lazy">
-              </iframe>
+            {/* Right Column */}
+            <Col xs={8} className="mt-5">
+              {/* First Row */}
+              <Row className="p-lg-5 bg-white rounded-5 shadow mb-5">
+                <Col xs={12}>
+                  <h2>{data[0].name}</h2>
+                </Col>
+                <Col xs={12}>
+                  <i className="bi bi-tag-fill text-warning me-3" style={{ fontSize: "1.5rem" }} />
+                  <p className="d-inline-block text-secondary text-capitalize" style={{ fontSize: "1.2rem" }}>
+                    {data[0].eventType}
+                  </p>
+                </Col>
+                <Col xs={12} className="rounded-4 p-4 mb-5" style={{ backgroundColor: "#f1f1f1" }}>
+                  <Row>
+                    <Col xs={12}>
+                      <h5>Event Description</h5>
+                    </Col>
+                    <Col xs={12}>
+                      {data[0].description}
+                    </Col>
+                  </Row>
+                </Col>
+                {/*<Col xs={12} className="mb-5">
+                  <button style={{ border: "none", borderRightStyle: "solid", fontSize: "20px" }} className="text-warning bg-white fw-bold ps-0 pe-5">Tickets</button>
+                  <button style={{ border: "none", borderRightStyle: "solid", fontSize: "20px" }} className="text-warning bg-white fw-bold px-5">Performers</button>
+                  <button style={{ border: "none", borderRightStyle: "solid", fontSize: "20px" }} className="text-warning bg-white fw-bold px-5">Rules</button>
+                </Col>*/}
+                <Col xs={12}>
+                  <Row>
+                    <Col xs={8}>
+                      <Button variant="outline-warning" size="lg" onClick={()=> setValue("tickets")}>
+                        Tickets
+                      </Button>
+                      <Button variant="outline-warning" size="lg" onClick={()=> setValue("performers")}>
+                        Performers
+                      </Button>
+                      <Button variant="outline-warning" size="lg" onClick={()=> setValue("rules")}>
+                        Rules
+                      </Button>
+                    </Col>
+                    <Col xs={4}>
+                      <Button variant="danger" size="lg">
+                        Add Favorities
+                        <i className="bi bi-heart-fill text-white ms-3" />
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+
+              {/* Second Row */}
+              <Row className="p-lg-5 bg-white rounded-5 shadow mb-5">
+                <Col xs={12}>
+                  <ShareButtons eventName={data[0].name} eventType={data[0].eventType} eventDescription={data[0].description} />
+                </Col>
+                <Col xs={12}>
+                  <iframe
+                    src={(googleMap.find(item => item.city.toLowerCase() === data[0].city.toLowerCase())).url}
+                    height="450"
+                    style={{ border: "0", width: "100%" }}
+                    loading="lazy">
+                  </iframe>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
       )}
-    </div>
+    </>
   )
 }
 
