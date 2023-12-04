cc5bf17285e0e7dd386daa644a58a35e08a8a3e9
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-12-04 22:18:27 +0300

    Performers and mapUrl properties have been added to the data. Errors in Breadcrumb component have been fixed.

--- src/Components/Breadcrumbs/index.js
+++ src/Components/Breadcrumbs/index.js
@@ -5,19 +5,15 @@ import { Link } from "react-router-dom";
 function Breadcrumbs({ eventName }) {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item>
-        <Link to="/">
-          <i class="bi bi-house-door-fill text-warning" style={{fontSize:"1.2rem"}}/>
-        </Link>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item>
-        <Link to="/" className="text-warning">
-          Etkinlikler
-        </Link>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item active>
+      <Link to="/" className="breadcrumb-item">
+        <i className="bi bi-house-door-fill text-warning" style={{ fontSize: "1.2rem" }} />
+      </Link>
+      <Link to="/" className="text-warning breadcrumb-item">
+        Etkinlikler
+      </Link>
+      <div className="breadcrumb-item active">
         {eventName}
-      </Breadcrumb.Item>
+      </div>
     </Breadcrumb>
   )
 }
